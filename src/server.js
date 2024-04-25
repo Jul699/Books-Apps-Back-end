4efb7e0ec7e53935f9d7a438b76e38e03103109f
Irfan Juliana <irfanjuliana099@gmail.com>
2024-04-25 07:30:11 +0700

    fix bug host

--- src/server.js
+++ src/server.js
@@ -2,23 +2,34 @@
 //3220302
 //Irfan Juliana
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes')
+const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
         port: 3003,
         host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
-        routes:{
-            cors:{
+        routes: {
+            cors: {
                 origin: ['*'],
             },
         },
-    })
+    });
 
-    server.route(routes)
+    server.route(routes);
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`)
-}
+    // Tangani event 'clientError'
+    server.listener.on('clientError', (err, socket) => {
+        console.error('Error pada koneksi client:', err);
+        // Hancurkan socket yang terkait
+        socket.destroy(err);
+    });
 
-init()
+    try {
+        await server.start();
+        console.log(`Server berjalan pada ${server.info.uri}`);
+    } catch (error) {
+        console.error('Gagal memulai server Hapi:', error);
+    }
+};
+
+init();
